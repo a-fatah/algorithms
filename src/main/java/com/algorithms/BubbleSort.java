eedf41e75734c845d68c324e5a206448f5845188
Abdul Fatah <abdulfatah66@gmail.com>
2021-04-17 20:40:14 +0500

    refactor

--- src/main/java/com/algorithms/BubbleSort.java
+++ src/main/java/com/algorithms/BubbleSort.java
@@ -4,23 +4,27 @@ import java.util.Arrays;
 import java.util.stream.IntStream;
 
 public class BubbleSort {
-    public static void main(String[] args) {
-        int[] array = {1,5,4,2};
-        bubbleSort(array);
-        System.out.println("Sorted Array: ");
-        System.out.println(Arrays.toString(array));
-    }
+  public static void main(String[] args) {
+    int[] array = {1,5,4,2};
+    bubbleSort(array);
+    System.out.println("Sorted Array: ");
+    System.out.println(Arrays.toString(array));
+  }
 
-    private static void bubbleSort(int[] array) {
-        int n = array.length;
-        IntStream.range(0, n - 1)
-            .flatMap(i -> IntStream.range(1, n - i))
-            .forEach(j -> {
-                if (array[j - 1] > array[j]) {
-                    int temp = array[j - 1];
-                    array[j - 1] = array[j];
-                    array[j] = temp;
-                }
-            });
-    }
+  private static void bubbleSort(int[] array) {
+    int n = array.length;
+    IntStream.range(0, n - 1)
+      .flatMap(i -> IntStream.range(1, n - i))
+      .forEach(j -> {
+        if (array[j - 1] > array[j]) {
+          swap(array, j - 1, j);
+        }
+      });
+  }
+
+  private static void swap(int[] arr, int i, int j) {
+    int temp = arr[i];
+    arr[i] = arr[j];
+    arr[j] = temp;
+  }
 }
