28a9494c43caf413576be87ab8d2416d9b407e51
Abdul Fatah <abdulfatah66@gmail.com>
2021-04-17 20:33:55 +0500

    add bubblesort

---
+++ src/main/java/com/algorithms/BubbleSort.java
@@ -0,0 +1,26 @@
+package com.algorithms;
+
+import java.util.Arrays;
+import java.util.stream.IntStream;
+
+public class BubbleSort {
+    public static void main(String[] args) {
+        int[] array = {1,5,4,2};
+        bubbleSort(array);
+        System.out.println("Sorted Array: ");
+        System.out.println(Arrays.toString(array));
+    }
+
+    private static void bubbleSort(int[] array) {
+        int n = array.length;
+        IntStream.range(0, n - 1)
+            .flatMap(i -> IntStream.range(1, n - i))
+            .forEach(j -> {
+                if (array[j - 1] > array[j]) {
+                    int temp = array[j - 1];
+                    array[j - 1] = array[j];
+                    array[j] = temp;
+                }
+            });
+    }
+}
